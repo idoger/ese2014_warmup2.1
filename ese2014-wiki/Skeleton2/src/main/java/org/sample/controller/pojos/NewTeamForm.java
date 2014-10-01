669b8514fa438023060f437f9700c76bd9c1d7ba
Ido Gerschoni <ido.gerschoni@kjgldjfdljgd>
2014-10-01 09:47:34 +0200

    commit

---
+++ ese2014-wiki/Skeleton2/src/main/java/org/sample/controller/pojos/NewTeamForm.java
@@ -0,0 +1,16 @@
+package org.sample.controller.pojos;
+
+public class NewTeamForm {
+	
+	private String team;
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	
+
+}
